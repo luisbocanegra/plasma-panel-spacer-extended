0eb7e521b80dcc65e5355336e4c46ef410c1783b
Luis Bocanegra <luisbocanegra@users.noreply.github.com>
2024-04-25 06:47:37 -0600

    If present, select the active window in the current screen https://github.com/luisbocanegra/plasma-panel-spacer-extended/issues/26

--- package/contents/ui/tools/focusTopWindow.js
+++ package/contents/ui/tools/focusTopWindow.js
@@ -9,22 +9,32 @@ function printLog(strings, ...values) {
   }
 }
 const windows = workspace.windowList();
-printLog`PPSE: Focusing top window...\n`
+printLog`Focusing top window...\n`
 var topWindow
 let stackPosition = -1
-
-
-var currentScreen = workspace.screenAt(workspace.cursorPos)
 // TODO is this needed at all if workspace.activeScreen should always have the screen with cursor?
+var currentScreen = workspace.screenAt(workspace.cursorPos)
+var currentDesktop = workspace.currentDesktop
 printLog`active screen: ${workspace.activeScreen.name} current: ${currentScreen.name}`
 for (var i = 0; i < windows.length; i++) {
-  if (windows[i].output === workspace.activeScreen && !windows[i].desktopWindow && windows[i].normalWindow && !windows[i].minimized && windows[i].moveable && !windows[i].hidden) {
-    if (windows[i].stackingOrder > stackPosition) {
-      topWindow = windows[i]
+  let window = windows[i]
+  if (window.output === workspace.activeScreen
+    && (window.onAllDesktops || window.desktops.includes(currentDesktop))
+    && !window.desktopWindow
+    && window.normalWindow
+    && !window.minimized
+    && !window.hidden
+  ) {
+    if (window.active) {
+      topWindow = window
+      break
     }
-    stackPosition = windows[i].stackingOrder
+    if (window.stackingOrder > stackPosition) {
+      topWindow = window
+    }
+    stackPosition = window.stackingOrder
   }
 }
 workspace.activeWindow = topWindow
-resut = ("Top: " + topWindow.caption + "|" + topWindow.resourceName + "|" + topWindow.resourceClass + "|" + topWindow.stackingOrder + "|" + topWindow.output.name)
+const resut = ("Top: " + topWindow.caption + "|" + topWindow.resourceName + "|" + topWindow.resourceClass + "|" + topWindow.stackingOrder + "|" + topWindow.output.name)
 printLog`${resut}`
